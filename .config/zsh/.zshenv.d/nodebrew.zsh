fe3924cfa759dc24c0d674dd282a2a7ac6520681
Ryume Taguchi <tgcrym01@gmail.com>
2022-10-21 12:59:27 +0900

    zsh: export `NODEBREW_ROOT`

--- .config/zsh/.zshenv.d/nodebrew.zsh
+++ .config/zsh/.zshenv.d/nodebrew.zsh
@@ -1,3 +1,4 @@
-if [ -d $HOME/.nodebrew/current/bin ]; then
-  export PATH=$HOME/.nodebrew/current/bin:$PATH
+if type nodebrew > /dev/null 2>&1; then
+  export NODEBREW_ROOT="$XDG_DATA_HOME/nodebrew"
+  export PATH=$NODEBREW_ROOT/current/bin:$PATH
 fi
