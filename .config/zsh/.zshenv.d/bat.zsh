b4a95869721e6b6230a3aabc581f995a6901cd07
Ryuma Taguchi <tgcrym01@gmail.com>
2023-09-11 22:05:09 +0900

    zsh: add nvm environment configuration

--- .config/zsh/.zshenv.d/bat.zsh
+++ .config/zsh/.zshenv.d/bat.zsh
@@ -1,3 +1,3 @@
-if type bat > /dev/null 2>&1; then
+if command -v bat > /dev/null 2>&1; then
     export BAT_THEME="TwoDark"
 fi
---
+++ .config/zsh/.zshenv.d/nvm.zsh
@@ -0,0 +1,2 @@
+export NVM_DIR="$XDG_DATA_HOME/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
